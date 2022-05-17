51cbf674fd879ac4d40d0e6e3b97cb12e5dfe763
zacharyrb99 <zach.boudreaux999@gmail.com>
2022-05-17 09:33:43 -0400

    added comments and updated README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Udundi Coding Challenge
+Mock website built for Udundi interview.
+
+**Tech used:**
+1. HTML5
+2. JavaScript (jQuery)
+3. SCSS (Live Sass Compiler extension for VSCode to map to CSS3)
+
+__Time to complete: 10 hours__
--- script.js
+++ script.js
@@ -3,17 +3,21 @@ let $modalBtn = $("#details-btn");
 let $closeBtn = $("#close");
 let $details = $("#details");
 
+// Open modal when clicking on details button
 $modalBtn.click(() => {
     $modal.show(1000);
     $details.hide(1000);
 });
 
+// Close modal when clicking on close button
 $closeBtn.click(() => {
     $modal.hide(1000);
     $details.show(1000);
 });
 
+// Close modal when clicking outside of modal
 $(document).click(e => {
+    // Checks whether you clicked on modal/details button
     if (e.target.closest("#modal") || e.target.closest("#details-btn")) return;
     
     $modal.hide(1000);
